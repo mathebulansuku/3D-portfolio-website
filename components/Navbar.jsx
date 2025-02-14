e6de91e5286084882bf75d05487bfc0052ee399f
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-02-14 13:41:49 +0200

    setActive state keeps track of where we are on the page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,11 +5,22 @@ import { NavLinks } from "../src/constants";
 import { logo, menu, close } from "../src/assets";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
   return (
     <nav
       className={`${styles.paddindX} w-full flex item-center py-5 fixed top-0 z-20 bg-primary`}
     >
-      <div className="w-ful flex justify-between items-center max-w-7xl mx-auto"></div>
+      <div className="w-ful flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center"
+          gap-2
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        ></Link>
+      </div>
     </nav>
   );
 };
